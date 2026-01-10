042f4633a644caec2989b15b0df0a3cc6c1d8387
Martinilli <martina.magyar07@gmail.com>
2026-01-10 18:10:40 +0100

    Dockerizálás (Kóté Máté + Magyar Martina)

--- users/app.js
+++ users/app.js
@@ -1,44 +1,28 @@
 const express = require("express");
-
-const app = express();
-
-const api = express();
-
 const cors = require("cors");
-
 const cookieParser = require("cookie-parser");
 
-app.use(cors(
-{
-    origin: "http://localhost:3000",
-    credentials: true 
-}));
+const app = express();
+const api = express();
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
 const userRoutes = require("./api/routes/userRoutes");
-
-const errorHandler = require("./api/middlewares/errorHandler");
-
 const authRoutes = require("./api/routes/authRoutes");
-
-const scoreRoutes = require("./api/routes/scoreRoutes")
+const scoreRoutes = require("./api/routes/scoreRoutes");
+const errorHandler = require("./api/middlewares/errorHandler");
 
 app.use("/api", api);
 
-api.use("/score",scoreRoutes)
-
 api.use("/users", userRoutes);
-
 api.use("/auth", authRoutes);
+api.use("/score", scoreRoutes);
 
+app.get("/health", (req, res) => res.sendStatus(200));
 
 app.use(errorHandler.showError);
-
-// app.use(errorHandler.notFound);
 
 module.exports = app;
